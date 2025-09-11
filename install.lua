61b124a9790fe2d7cc7def0b941e206af4f64f31
DeadlyBraincell <zaeh.philip@gmail.com>
2025-09-11 22:00:18 +0200

    Fixed mismatch between install and startup

--- install.lua
+++ install.lua
@@ -1,7 +1,9 @@
 local root = "https://raw.githubusercontent.com/DeadlyBraincell/Computercraft_Server_util/main/"
 
 shell.run("wget "..root.."startup.lua")
+fs.makeDir("/Computercraft_Serverutil")
 shell.run("cd Computercraft_Serverutil")
 shell.run("wget "..root.."Computercraft_Serverutil/join_message.lua")
 shell.run("wget "..root.."Computercraft_Serverutil/weather_controller.lua")
 shell.run("cd ..")
+--"wget https://raw.githubusercontent.com/DeadlyBraincell/Computercraft_Server_util/main/install.lua"
